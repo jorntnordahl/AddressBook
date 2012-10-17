665c06850defd7e5aca83db760678714773c0f15
Bill Culp <gitmonster@gmx.com>
2012-10-16 22:39:06 -0700

    annotation

--- AddressBook.h
+++ AddressBook.h
@@ -6,12 +6,25 @@
 //  Copyright (c) 2012 Jorn Nordahl. All rights reserved.
 //
 
+//Jorn just putting some questions in comments - I know this is out of a book
+//so I just want to annotate some thoughts on the code.
+
+
 #import <Foundation/Foundation.h>
 #import "AdressCard.h"
 
 @interface AddressBook : NSObject
 
+//NSString is an immutable object why copy it?
 @property (nonatomic, copy) NSString *bookName;
+
+//exposing the book as an array is ok saves you having to create your own iterator
+//but it should be readonly in general to allow the mutator methods to do the mutations
+//also you want ot expose it as NSArray externally even though its actually mutable internally
+//yes a dev can cast around it but thats a bad practice
+//so maybe instead @property(nonatomic, retain, readonly, getter=book) NSArray *book;
+//there will be no setter and you can create the internal NSMutableArray * as a field
+//and return it as NSArray in your custom getter
 @property (nonatomic, strong) NSMutableArray *book;
 
 -(id) initWithName: (NSString *) name;
